8b7cba72d8157763dd0e62739cd3971a881a0986
sev7ves <soapedzone@gmail.com>
2026-04-14 21:43:28 -0400

    yuuup

--- Content.Server/Atmos/EntitySystems/AtmosphereSystem.Commands.cs
+++ Content.Server/Atmos/EntitySystems/AtmosphereSystem.Commands.cs
@@ -36,7 +36,7 @@ public sealed partial class AtmosphereSystem
            return;
        }
 
-       var mixtures = new GasMixture[13]; // Add one per added array. // Frontier:9<13
+       var mixtures = new GasMixture[15]; // Add one per added array. // Frontier:9<13 //Aurora:13<15
        for (var i = 0; i < mixtures.Length; i++)
            mixtures[i] = new GasMixture(Atmospherics.CellVolume) { Temperature = Atmospherics.T20C };
 
@@ -86,6 +86,12 @@ public sealed partial class AtmosphereSystem
        mixtures[12].AdjustMoles(Gas.Nitrogen, Atmospherics.NitrogenMolesStandard);
        mixtures[12].AdjustMoles(Gas.WaterVapor, Atmospherics.NitrogenMolesStandard);
        mixtures[12].Temperature = 340f; // Sauna
+
+       // Aurora - 13: Respiron @ 101kpa, for the motel
+       mixtures[13].AdjustMoles(Gas.Respiron, Atmospherics.MolesCellStandard);
+
+       // Aurora - 14: Respiron (GM), for the motel
+       mixtures[14].AdjustMoles(Gas.Respiron, Atmospherics.MolesCellStandard);
 
        foreach (var arg in args)
        {
